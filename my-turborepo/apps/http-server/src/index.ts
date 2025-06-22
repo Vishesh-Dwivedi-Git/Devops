ef0eae039fdf7582b4964d7e0e7b5b720b230fc8
Vishesh Dwivedi <visheshdwivedi225544@gmail.com>
2025-06-22 05:52:25 +0530

    feat:prisma added and slight changes

--- my-turborepo/apps/http-server/src/index.ts
+++ my-turborepo/apps/http-server/src/index.ts
@@ -1,7 +1,27 @@
 import express from 'express';
-
+import {client} from '@repo/db/client';
 const app=express();
 
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello from HTTP server");
+});
+
+app.post( "/signup",async (req , res)=>{
+      const {name  , password} = req.body; 
+     const user= await client.user.create({
+            data:{
+                 name, 
+                 password
+            }
+     })
+     res.json({
+         message:"User Created Successfully",
+        id: user.id, 
+     })
+    
+})
 app.listen(3000,()=>{
      console.log("server is running on port 3000");
 })
